3682329ecfb80964f0c06b7136e7d8ed91c71fde
Bobby Connolly <bobbyconnolly@gmail.com>
2022-01-22 11:52:32 -0500

    ready to start coding

--- students/Bobby/hello-cra/src/App.js
+++ students/Bobby/hello-cra/src/App.js
@@ -1,5 +1,7 @@
-function App() {
+export function App() {
     return <div className="App">Taco</div>
 }
 
-export default App
+export function AppFooter() {
+    return <div>This is the footer</div>
+}
