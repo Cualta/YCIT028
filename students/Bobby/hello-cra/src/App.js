ffbd7e1d68fb791b67f4765c7fad1e523a3b480c
Bobby Connolly <bobbyconnolly@gmail.com>
2022-01-22 12:40:16 -0500

    add header

--- students/Bobby/hello-cra/src/App.js
+++ students/Bobby/hello-cra/src/App.js
@@ -1,7 +1,45 @@
+import "./App.styles.css"
+
 export function App() {
-    return <div className="App">Taco</div>
+    return (
+        <div className="App">
+            <Header />
+            <Main />
+            <Footer />
+        </div>
+    )
 }
 
-export function AppFooter() {
-    return <div>This is the footer</div>
+function Header() {
+    return (
+        <div className="Header">
+            <NavBar />
+        </div>
+    )
+}
+
+function NavBar() {
+    return (
+        <div className="NavBar">
+            <NavBarItem text="Home" />
+            <NavBarItem text="Products" />
+            <NavBarItem text="About" />
+        </div>
+    )
+}
+
+function NavBarItem(props) {
+    return (
+        <a onClick={() => alert("Hello?")}>
+            <div className="NavBarItem">{props.text}</div>
+        </a>
+    )
+}
+
+function Main() {
+    return <div className="Main"></div>
+}
+
+function Footer() {
+    return <div className="Footer"></div>
 }
